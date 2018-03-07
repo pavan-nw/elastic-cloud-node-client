9e94368f7938d8e96218a7ffc69be2e13dc0c781
pavannw <pavansdoc@gmail.com>
2018-03-07 23:22:00 +0530

    search api trials

--- elastic-client.js
+++ elastic-client.js
@@ -3,7 +3,8 @@ const elasticConfig = require('./elastic-config');
 
 const client = new elasticsearch.Client({
   host: elasticConfig.host,
-  httpAuth: elasticConfig.httpAuth
+  httpAuth: elasticConfig.httpAuth,
+  log: 'error'
 });
 
 module.exports = client;
--- routes/search.js
+++ routes/search.js
@@ -4,8 +4,23 @@ const client = require('../elastic-client');
 const router = express.Router();
 
 router.get('/events', (req, res) => {
+  const queryString = req.query.q || '';
+  console.log(queryString);
+
   client
-    .search({ index: 'events' })
+    .search({
+      index: 'events',
+      // q: req.query.q
+      body: {
+        query: {
+          simple_query_string: {
+            query: queryString,
+            fields: ['title^2', 'tagLine^5'],
+            default_operator: 'or'
+          }
+        }
+      }
+    })
     .then(results => res.json(results))
     .catch(error => res.json(error));
 });
